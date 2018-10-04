43d053cc3529bf8f527417b0f2ca7bfa1fe97e94
Lyn Elisa Goltz <goltz@lat-lon.de>
2018-10-04 14:36:07 +0200

    #43 - replaced random limits

--- src/main/java/org/opengis/cite/wfs30/collections/GetFeaturesOperation.java
+++ src/main/java/org/opengis/cite/wfs30/collections/GetFeaturesOperation.java
@@ -32,7 +32,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 import org.opengis.cite.wfs30.CommonDataFixture;
 import org.opengis.cite.wfs30.SuiteAttribute;
@@ -92,9 +91,17 @@ public class GetFeaturesOperation extends CommonDataFixture {
             List<TestPoint> testPoints = retrieveTestPointsForCollection( apiModel, collectionName );
             for ( TestPoint testPoint : testPoints ) {
                 Parameter limit = findParameterByName( testPoint, "limit" );
-                int[] ints = getTwoRandomLimits( limit );
-                collectionsWithLimits.add( new Object[] { collection, ints[0] } );
-                collectionsWithLimits.add( new Object[] { collection, ints[0] } );
+                if ( limit != null && limit.getSchema() != null ) {
+                    int min = limit.getSchema().getMinimum().intValue();
+                    int max = limit.getSchema().getMaximum().intValue();
+                    if ( min == max ) {
+                        collectionsWithLimits.add( new Object[] { collection, min } );
+                    } else {
+                        collectionsWithLimits.add( new Object[] { collection, min } );
+                        int betweenMinAndMax = min + ( ( max - min ) / 2 );
+                        collectionsWithLimits.add( new Object[] { collection, betweenMinAndMax } );
+                    }
+                }
             }
         }
         return collectionsWithLimits.iterator();
@@ -761,16 +768,6 @@ public class GetFeaturesOperation extends CommonDataFixture {
     private void assertIntegerGreaterZero( int value, String propertyName ) {
         String msg = "Expected property '%s' to be an integer greater than 0, but was '%s'";
         assertTrue( value > 0, String.format( msg, propertyName, value ) );
-    }
-
-    private int[] getTwoRandomLimits( Parameter limit ) {
-        Schema schema = limit.getSchema();
-        int min = schema.getMinimum().intValue();
-        int max = schema.getMaximum().intValue();
-        if ( max > 25 )
-            max = 25;
-        Random random = new Random();
-        return random.ints( 2, min, max ).toArray();
     }
 
     private class ResponseData {
